653bb5c38e934571f274227bc64a76f92e0cf1f1
Luke Sands <luke.sands@d3code.com.au>
2022-06-19 04:40:07 +1000

    Updates

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/d3code/common-go
+
+go 1.17
+
+require (
+)
---
+++ helper/rsa.go
@@ -0,0 +1,74 @@
+package helper
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
+
+func GenerateRsaKeyPair() (*rsa.PrivateKey, *rsa.PublicKey) {
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 4096)
+	return privateKey, &privateKey.PublicKey
+}
+
+func ExportRsaPrivateKeyAsPemStr(privateKey *rsa.PrivateKey) string {
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	encodeToMemory := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privateKeyBytes,
+		},
+	)
+	return string(encodeToMemory)
+}
+
+func ParseRsaPrivateKeyFromPemStr(privateKeyPem string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKeyPem))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func ExportRsaPublicKeyAsPemStr(publicKey *rsa.PublicKey) (string, error) {
+	pkixPublicKey, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return "", err
+	}
+	encodeToMemory := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pkixPublicKey,
+		},
+	)
+
+	return string(encodeToMemory), nil
+}
+
+func ParseRsaPublicKeyFromPemStr(publicKey string) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(publicKey))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	pkixPublicKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub := pkixPublicKey.(type) {
+	case *rsa.PublicKey:
+		return pub, nil
+	default:
+		break
+	}
+	return nil, errors.New("key type is not RSA")
+}

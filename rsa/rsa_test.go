1bbd85ecd12aac8784cef5e24d4ec6d17bf8bee6
Luke Sands <luke.sands@d3code.com.au>
2022-06-19 12:22:56 +1000

    Updates

--- rsa/rsa_test.go
+++ rsa/rsa_test.go
@@ -1,6 +1,7 @@
 package rsa
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -12,17 +13,62 @@ func TestGenerateRsaKeyPair(t *testing.T) {
 }
 
 func TestExportRsaPrivateKeyAsPemStr(t *testing.T) {
+	privateKey, _ := GenerateRsaKeyPair()
+	privateKeyAsPemStr := ExportRsaPrivateKeyAsPemStr(privateKey)
 
+	if !strings.HasPrefix(privateKeyAsPemStr, "-----BEGIN RSA PRIVATE KEY-----") {
+		t.Error("Pem block does not start with correct string")
+	}
+
+	if !strings.HasSuffix(privateKeyAsPemStr, "-----END RSA PRIVATE KEY-----\n") {
+		t.Error("Pem block does not end with correct string")
+	}
 }
 
 func TestParseRsaPrivateKeyFromPemStr(t *testing.T) {
+	privateKey, _ := GenerateRsaKeyPair()
+	privateKeyAsPemStr := ExportRsaPrivateKeyAsPemStr(privateKey)
 
+	privateKeyFromPemStr, err := ParseRsaPrivateKeyFromPemStr(privateKeyAsPemStr)
+
+	if err != nil {
+		t.Errorf("Error from parsing string: %s", err)
+	}
+
+	if privateKey.E != privateKeyFromPemStr.E {
+		t.Error("Private keys not the same after string conversion")
+	}
 }
 
 func TestExportRsaPublicKeyAsPemStr(t *testing.T) {
+	_, publicKey := GenerateRsaKeyPair()
+	publicKeyAsPemStr, err := ExportRsaPublicKeyAsPemStr(publicKey)
+	if err != nil {
+		t.Errorf("Error from exporting public key as string: %s", err)
+	}
 
+	if !strings.HasPrefix(publicKeyAsPemStr, "-----BEGIN RSA PUBLIC KEY-----") {
+		t.Error("Pem block does not start with correct string")
+	}
+
+	if !strings.HasSuffix(publicKeyAsPemStr, "-----END RSA PUBLIC KEY-----\n") {
+		t.Error("Pem block does not end with correct string")
+	}
 }
 
 func TestParseRsaPublicKeyFromPemStr(t *testing.T) {
+	_, publicKey := GenerateRsaKeyPair()
+	publicKeyAsPemStr, err := ExportRsaPublicKeyAsPemStr(publicKey)
+	if err != nil {
+		t.Errorf("Error from exporting public key as string: %s", err)
+	}
 
+	publicKeyFromPemStr, err := ParseRsaPublicKeyFromPemStr(publicKeyAsPemStr)
+	if err != nil {
+		t.Errorf("Error from parsing string: %s", err)
+	}
+
+	if publicKey.E != publicKeyFromPemStr.E {
+		t.Error("Private keys not the same after string conversion")
+	}
 }
